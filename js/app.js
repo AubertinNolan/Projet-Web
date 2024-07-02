e2a909f972899b5549373e46f9601d44ec8ad297
Aubertin Nolan <84779468+AubertinNolan@users.noreply.github.com>
2024-07-02 21:47:53 +0200

    ajout connexion/inscription sans php

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("JS a bien chargé");
+
+/* var maTable = "<table border='1'>";
+
+for (var i = 1; i <= 5; i++) {
+  maTable += "<tr>";
+
+  for (var j = 1; j <= 10; j++) {
+    maTable += "<td> " + j + "</td>";
+  }
+
+  maTable += "</tr>";
+}
+
+maTable += "</table>";
+
+document.write(maTable); */
